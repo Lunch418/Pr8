b0d22e894e1b198b5b16257919b7b0a68624b159
Артем Сыпачев <Lunch418@bk.ru>
2025-12-16 14:44:45 +0500

    GetWeatger.Сыпачев

---
+++ Classes/GetWeather.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System.Net.Http;
+using Weather.Models;
+
+
+namespace Weather.Classes
+{
+    public class GetWeather
+    {
+        public static string Url = "https://api.weather.yandex.ru/v2/forecast";
+        public static string Key = "demo.yandex_weather.api.key_code0893M9bao";
+
+        public static async Task<string> Get(float lat, float lon)
+        {
+            string requestUrl = $"{Url}?lat={lat}&lon={lon}".Replace(",", ".");
+
+            using (HttpClient client = new HttpClient())
+            {
+                using (HttpRequestMessage request = new HttpRequestMessage(
+                    HttpMethod.Get,
+                    requestUrl))
+                {
+                    request.Headers.Add("X-Yandex-Weather-Key", Key);
+
+                    using (var response = await client.SendAsync(request))
+                    {
+                        string dataResponse = await response.Content.ReadAsStringAsync();
+                        return dataResponse;
+                    }
+                }
+            }
+        }
+    }
+}
