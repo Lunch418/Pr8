f9aa03e89b04753688438db0caa6bd85d3399062
Артем Сыпачев <Lunch418@bk.ru>
2025-12-16 15:05:05 +0500

    Datea Response.Сыпачев

--- Models/DateResponse.cs
+++ Models/DateResponse.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace Weather.Models
 {
-    class DateResponse
+    public class DataResponse
     {
+        public List<Forecast> forecasts { get; set; }
+    }
+
+    public class Forecast
+    {
+        public DateTime date { get; set; }
+        public List<Hour> hours { get; set; } 
+    }
+
+    public class Hour
+    {
+        public string hour { get; set; } 
+        public string condition { get; set; }
+        public int humidity { get; set; }
+        public int precType { get; set; }
+        public int temp { get; set; }
     }
 }
